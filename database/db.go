95e227b1ecb689d625f4d54974843b405cc271a9
Ildar Magzhanov <ildar200103@rambler.ru>
2023-10-16 17:20:34 +0300

    cleaning up

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ func GetDB() (*sql.DB, error) {
 	if db == nil {
 		db, err = sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s "+
 			"password=%s dbname=%s sslmode=disable",
-			config.Host, config.Port, config.User, config.Password, config.Dbname)) // create db_config.go file with these constants
+			config.Host, config.Port, config.User, config.Password, config.Dbname)) // create config/db_config.go file with these constants
 		if err != nil {
 			return nil, err
 		}
