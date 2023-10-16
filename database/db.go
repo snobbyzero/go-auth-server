adc1eb75896691ff51092fc3c7e38f0acb36499d
Ildar Magzhanov <ildar200103@rambler.ru>
2023-10-16 17:18:58 +0300

    added configuration file

--- database/db.go
+++ database/db.go
@@ -1,13 +1,20 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"go_auth_server/config"
+)
+
 
 var db *sql.DB
 
 func GetDB() (*sql.DB, error) {
 	var err error
 	if db == nil {
-		db, err = sql.Open("postgres", "user=postgres password=postgres dbname=go-auth-server sslmode=disable")
+		db, err = sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			config.Host, config.Port, config.User, config.Password, config.Dbname)) // create db_config.go file with these constants
 		if err != nil {
 			return nil, err
 		}
